db3a0699cb89e8cbff0663ea64da7d16d5521512
jim nist <james.nist@sonder.com>
2023-02-19 09:33:36 -0800

    moar

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -20,9 +20,9 @@ git_dirty() {
   else
     if [[ $($git status --porcelain) == "" ]]
     then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+      echo "[%{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}]"
     else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+      echo "[%{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}]"
     fi
   fi
 }
@@ -51,22 +51,10 @@ need_push () {
 }
 
 directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+  echo "%{$fg_bold[cyan]%}%1/%\%{$reset_color%}"
 }
 
-battery_status() {
-  if test ! "$(uname)" = "Darwin"
-  then
-    exit 0
-  fi
-
-  if [[ $(sysctl -n hw.model) == *"Book"* ]]
-  then
-    $ZSH/bin/battery-status
-  fi
-}
-
-export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(directory_name) $(git_dirty)$(need_push)\n%{$fg_bold[green]%}›: %{$reset_color%}'
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
